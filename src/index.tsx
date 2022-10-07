06f80e05d4a8c8f1c2d019248d430f7d0b30036c
EAGzzyCSL <eagzzycsl@hotmail.com>
2022-10-07 20:17:30 +0800

    chore: update all deps

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-
 import './index.css'
+import { createRoot } from 'react-dom/client'
+
 import App from './App'
 
-ReactDOM.render(
+const container = document.getElementById('root')
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(container!)
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
 )
